a00115b2193fac736738110372fae753b0a7e85f
Pablo Perea Campos <pablopereac02@gmail.com>
2025-12-09 18:37:06 +0100

    add: Repartidores entity

---
+++ src/main/java/com/autorizame/models/entity/Repartidores.java
@@ -0,0 +1,73 @@
+package com.autorizame.models.entity;
+
+public class Repartidores {
+
+	private Long id;
+	private String nombre;
+	private String correo;
+	private String telefono;
+	private Long empresaId;
+	private String estado;
+	
+	public Repartidores() {}
+
+	public Repartidores(Long id, String nombre, String correo, String telefono, Long empresaId, String estado) {
+		this.id = id;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.empresaId = empresaId;
+		this.estado = estado;
+	}
+	
+	// Getters y Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Long getEmpresaId() {
+		return empresaId;
+	}
+
+	public void setEmpresaId(Long empresaId) {
+		this.empresaId = empresaId;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
